010185988601d33f60cf6dc6a287c4d08a6d60f5
jherd801 <jherd801@gmail.com>
2020-07-30 09:52:01 -0600

    Removed build directory from gitignore

---
+++ build/precache-manifest.62bf507c716c0610c43daa324a4abe26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6f4b6a6901d9c08dba45cd40f15e86",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "b63b60edc9ece82dd477",
+    "url": "/reactPortfolio/static/css/main.e9b9998d.chunk.css"
+  },
+  {
+    "revision": "33ed0a17021ea09836bf",
+    "url": "/reactPortfolio/static/js/2.67ceeee0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPortfolio/static/js/2.67ceeee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63b60edc9ece82dd477",
+    "url": "/reactPortfolio/static/js/main.d7890a03.chunk.js"
+  },
+  {
+    "revision": "2f4d685e618f546a2f8a",
+    "url": "/reactPortfolio/static/js/runtime-main.506f4783.js"
+  },
+  {
+    "revision": "245aa9fd37ff5a6ccae2ca90eb864d79",
+    "url": "/reactPortfolio/static/media/HSE.245aa9fd.jpg"
+  },
+  {
+    "revision": "48a79dba8857022c9700866daa9a7d02",
+    "url": "/reactPortfolio/static/media/Web.48a79dba.png"
+  },
+  {
+    "revision": "7dca6e371b41905128189eed5ed71860",
+    "url": "/reactPortfolio/static/media/resume.7dca6e37.pdf"
+  }
+]);
